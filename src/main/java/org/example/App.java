6ce1165ac66939cbb2e5555365bfe396215e3f6e
Joseph Schander <josephSchander@gmail.com>
2021-09-10 10:18:57 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class App
+{
+
+    public static void main( String[] args )
+    {
+        Scanner nameOfUser = new Scanner(System.in);
+        System.out.println( "What is the quote?" );
+        String Quote = nameOfUser.nextLine();
+        System.out.println( "Who said it?");
+        String Name = nameOfUser.nextLine();
+        System.out.println(Name+" says,\" "+ Quote +"\"");
+    }
+}
